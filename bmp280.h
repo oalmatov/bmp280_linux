6a1b456d620e60357aa4c20e02176c7a644771cd
Omar Almatov <omar.almatov@gmail.com>
2019-06-30 16:27:33 +0600

    Initial commit

--- bmp280.h
+++ bmp280.h
@@ -1,7 +1,22 @@
-// bmp280.h
+/* bmp280.h
+****************************************************************************************************************
+****************************************************************************************************************
+    Copyright (C) 2019 Omar Almatov
+     
+    This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+    Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+    
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+    implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+    License for more details.
+    
+    You should have received a copy of the GNU General Public License along with this program. If not, see
+    <http://www.gnu.org/licenses/>.
+*/
 
-#ifndef BMP280_H
-#define BMP280_H 1
+#ifndef BMP280_H_
+#define BMP280_H_ 1
 
 #include <iostream>
 #include <fcntl.h>
@@ -44,4 +59,4 @@ protected:
     unsigned                adcPres_;
 };
 
-#endif // BMP280_H
+#endif // BMP280_H_
